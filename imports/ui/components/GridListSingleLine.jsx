298c3d37f84e0387183be3aabda4fc14f42e0226
Roman Brito <romanbrito1@gmail.com>
2017-07-02 20:03:14 -0500

    turned drawer to child

--- imports/ui/components/GridListSingleLine.jsx
+++ imports/ui/components/GridListSingleLine.jsx
@@ -1,8 +1,74 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+class GridListSingleLine extends Component {
+
+  state = {
+    open: false
+  };
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  getClick = (title) => {
+    console.log(title);
+  };
+
+  render() {
+    return (
+      <div className="container">
+
+        <div style={styles.root}>
+          <h1>{this.props.someTitle}</h1>
+
+          <MuiThemeProvider>
+            <GridList style={styles.gridList} cols={2.2}>
+              {tilesData.map((tile) => (
+                <GridTile
+                  key={tile.img}
+                  title={tile.title}
+                  actionIcon={<IconButton><StarBorder color="rgb(0, 188, 212)"/></IconButton>}
+                  titleStyle={styles.titleStyle}
+                  titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                >
+                  <img src={tile.img}
+                       onClick={() => {
+                         this.getClick(tile.title)
+                       }}/>
+                </GridTile>
+              ))}
+            </GridList>
+          </MuiThemeProvider>
+        </div>
+
+        <MuiThemeProvider>
+          <RaisedButton
+            label="New Order"
+            onTouchTap={this.handleToggle}
+          />
+        </MuiThemeProvider>
+
+        <MuiThemeProvider>
+          <Drawer open={this.state.open}>
+            <Link to="/">
+              <MenuItem>Close</MenuItem>
+            </Link>
+            <MenuItem>Menu Item</MenuItem>
+            <MenuItem>Menu Item 2</MenuItem>
+          </Drawer>
+        </MuiThemeProvider>
+
+      </div>
+    );
+
+  }
+}
+export default GridListSingleLine;
 
 // Grid
 const styles = {
@@ -66,65 +132,3 @@ const tilesData = [
 /**
  * This example demonstrates the horizontal scrollable single-line grid list of images.
  */
-//const getClick = (title) => console.log("hello click " + title);
-
-// const GridListSingleLine = (props) => (
-//
-//   <div style={styles.root}>
-//     <h1>{props.someTitle}</h1>
-//     <MuiThemeProvider>
-//     <GridList style={styles.gridList} cols={2.2}>
-//       {tilesData.map((tile) => (
-//         <GridTile
-//           key={tile.img}
-//           title={tile.title}
-//           actionIcon={<IconButton><StarBorder color="rgb(0, 188, 212)" /></IconButton>}
-//           titleStyle={styles.titleStyle}
-//           titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-//         >
-//           <img src={tile.img}
-//           onClick={() => {getClick(tile.title)}}
-//           />
-//         </GridTile>
-//       ))}
-//     </GridList>
-//     </MuiThemeProvider>
-//   </div>
-// );
-
-class GridListSingleLine extends Component {
-  getClick = (title) => {
-    console.log(title);
-  };
-
-  render() {
-    return (
-      <div style={styles.root}>
-        <h1>{this.props.someTitle}</h1>
-        <MuiThemeProvider>
-          <GridList style={styles.gridList} cols={2.2}>
-            {tilesData.map((tile) => (
-              <GridTile
-                key={tile.img}
-                title={tile.title}
-                actionIcon={<IconButton><StarBorder color="rgb(0, 188, 212)"/></IconButton>}
-                titleStyle={styles.titleStyle}
-                titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-              >
-                <img src={tile.img}
-                     onClick={() => {
-                       this.getClick(tile.title)
-                     }}/>
-              </GridTile>
-            ))}
-          </GridList>
-        </MuiThemeProvider>
-      </div>
-
-
-
-    );
-
-  }
-}
-export default GridListSingleLine;
--- imports/ui/components/orderNow.jsx
+++ imports/ui/components/orderNow.jsx
@@ -1,47 +1,13 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
+
 import GridListSingleLine from './GridListSingleLine';
 
 export default class orderNow extends Component {
-
-  state = {
-    open: false,
-    order: '',
-  };
-
-  handleToggle = () => this.setState({open: !this.state.open});
-
-  getClick = () => console.log("hello click");
 
 
   render() {
     return (
       <div>
-        
-        <MuiThemeProvider>
-          <RaisedButton
-            label="New Order"
-            onTouchTap={this.handleToggle}
-          />
-        </MuiThemeProvider>
-
-        <MuiThemeProvider>
-          <Drawer open={this.state.open}>
-            <Link to="/">
-              <MenuItem>Close</MenuItem>
-            </Link>
-            <MenuItem>Menu Item</MenuItem>
-            <MenuItem>Menu Item 2</MenuItem>
-          </Drawer>
-        </MuiThemeProvider>
-
-        {/*<img src="https://lorempixel.com/400/400/food/1" alt="food"*/}
-             {/*onClick={this.getClick}*/}
-        {/*/>*/}
 
         <GridListSingleLine someTitle="hello from order now"/>
 
